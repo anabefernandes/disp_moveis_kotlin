db538d713dd83c3587a157eee9fdced5ca231e08
Ana Beatriz <abeatriz.fernandes@outlook.com>
2025-03-16 09:50:08 -0300

    Segunda parte das tarefas

--- CalculoImc/app/src/main/java/br/edu/fatecpg/calculoimc/CalculoActivity.kt
+++ CalculoImc/app/src/main/java/br/edu/fatecpg/calculoimc/CalculoActivity.kt
@@ -1,20 +1,38 @@
 package br.edu.fatecpg.calculoimc
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class CalculoActivity : AppCompatActivity() {
+class CalculoActivity : AppCompatActivity(R.layout.activity_calculo) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_calculo)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val fabSair = findViewById<Button>(R.id.fab_sair)
+        val txtResultado = findViewById<TextView>(R.id.txt_resultado)
+        val txtImc = findViewById<TextView>(R.id.txt_imc)
+
+        val imc = intent.getDoubleExtra("imc", 0.0)
+        txtResultado.text = imc.toString()
+        if (imc < 18.5) {
+            txtImc.text = "Abaixo do peso."
+        } else if (imc == 18.5 || imc < 25) {
+            txtImc.text = "Peso normal."
+        } else if (imc == 25.toDouble() || imc < 30) {
+            txtImc.text = "Sobrepeso."
+        } else if (imc == 30.toDouble() || imc < 40) {
+            txtImc.text = "Obesidade."
+        } else {
+            txtImc.text = "Obesidade grave."
+        }
+
+        fabSair.setOnClickListener {
+            finish()
         }
     }
 }
--- CalculoImc/app/src/main/java/br/edu/fatecpg/calculoimc/MainActivity.kt
+++ CalculoImc/app/src/main/java/br/edu/fatecpg/calculoimc/MainActivity.kt
@@ -1,20 +1,41 @@
 package br.edu.fatecpg.calculoimc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val txtAltura = findViewById<TextView>(R.id.txt_altura)
+        val txtPeso = findViewById<TextView>(R.id.txt_peso)
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener{
+            val altura = txtAltura.text.toString().toDouble()
+            val peso = txtPeso.text.toString().toDouble()
+            if (altura != 0.0 || peso != 0.0) {
+                val imc = peso / (altura * altura)
+                val intent = Intent(this, CalculoActivity::class.java)
+                intent.putExtra("imc", imc)
+                startActivity(intent)
+            } else {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Erro")
+                builder.setMessage("Preencha os campos para o calculo.")
+                val dialog = builder.create()
+                dialog.show()
+            }
+
         }
+
     }
 }
